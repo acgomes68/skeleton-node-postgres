bd1c826a1dc99a0aa0322be813dae0b5b0c69fb4
acgomes68 <acgomes68@gmail.com>
2020-09-26 02:23:35 -0300

    comentário de warning do eslint

--- src/server.js
+++ src/server.js
@@ -5,5 +5,6 @@ import app from './app';
 const { APP_HOST, APP_PORT } = process.env;
 
 app.listen(APP_PORT, APP_HOST, () =>
+    // eslint-disable-next-line no-console
     console.log(`Now browse to ${APP_HOST}:${APP_PORT}/graphql`)
 );
